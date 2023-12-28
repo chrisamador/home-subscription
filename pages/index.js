d81921faa98e47bc839943c91d2ca405f7f5d99c
Chris Amador <Cja1218@gmail.com>
2023-12-28 02:08:14 +0000

    fix: get buttons to work

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,36 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import { useEffect, useRef } from 'react'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 export default function Home() {
   const ref = useRef(null);
   useEffect(() => {
-    if(ref.current) return;
+    if (ref.current) return;
     paypal.Buttons({
       style: {
-          shape: 'rect',
-          color: 'gold',
-          layout: 'vertical',
-          label: 'subscribe'
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'subscribe'
       },
-      createSubscription: function(data, actions) {
+      createSubscription: function (data, actions) {
         return actions.subscription.create({
           /* Creates the subscription */
           plan_id: 'P-8A254628MV0166352MWGL2XQ'
         });
       },
-      onApprove: function(data, actions) {
+      onApprove: function (data, actions) {
         alert(data.subscriptionID); // You can add optional success message for the subscriber here
       }
-  }).render('#paypal-button-container-P-8A254628MV0166352MWGL2XQ');
-  ref.current = true;
+    }).render('#paypal-button-container-P-8A254628MV0166352MWGL2XQ');
+    ref.current = true;
   }, [])
   return (
     <div className="container">
       <Head>
         <title>Living with Chris</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://www.paypal.com/sdk/js?client-id=AdmE03bjZ3MWYmIEk_3dlqeJ4xc6fCia1SCJBmn9RAz_STEgdTcuThpbO942ENSwJtTfzQ3bYVniDfNI&vault=true&intent=subscription" data-sdk-integration-source="button-factory"/>
       </Head>
 
       <main>
@@ -38,9 +38,17 @@ export default function Home() {
         <p className="description">
           Never miss a rent payment again. Subscribe below
         </p>
-        <div id="paypal-button-container-P-8A254628MV0166352MWGL2XQ"></div>
+
+        <div dangerouslySetInnerHTML={{ __html: `<div id="paypal-button-container-P-8A254628MV0166352MWGL2XQ"></div>` }} />
+        <PayPalScriptProvider options={{ "client-id": "AdmE03bjZ3MWYmIEk_3dlqeJ4xc6fCia1SCJBmn9RAz_STEgdTcuThpbO942ENSwJtTfzQ3bYVniDfNI&vault=true&intent=subscription" }}>
+          <PayPal />
+        </PayPalScriptProvider>
       </main>
 
     </div>
   )
+}
+
+function PayPal() {
+  return null
 }
