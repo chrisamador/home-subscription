749fe692b49d4578fbb9827d59d87c7a3afb2190
Chris Amador <Cja1218@gmail.com>
2023-12-28 01:37:23 +0000

    feat: subscribe buttons

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,44 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Living with Chris</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script src="https://www.paypal.com/sdk/js?client-id=AdmE03bjZ3MWYmIEk_3dlqeJ4xc6fCia1SCJBmn9RAz_STEgdTcuThpbO942ENSwJtTfzQ3bYVniDfNI&vault=true&intent=subscription" data-sdk-integration-source="button-factory"/>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to 10226 Stratford Ave" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Never miss a rent payment again. Subscribe below
         </p>
+        <div id="paypal-button-container-P-8A254628MV0166352MWGL2XQ"></div>
+        <script dangerouslySetInnerHTML={{__html: `
+        paypal.Buttons({
+          style: {
+              shape: 'rect',
+              color: 'gold',
+              layout: 'vertical',
+              label: 'subscribe'
+          },
+          createSubscription: function(data, actions) {
+            return actions.subscription.create({
+              /* Creates the subscription */
+              plan_id: 'P-8A254628MV0166352MWGL2XQ'
+            });
+          },
+          onApprove: function(data, actions) {
+            alert(data.subscriptionID); // You can add optional success message for the subscriber here
+          }
+      }).render('#paypal-button-container-P-8A254628MV0166352MWGL2XQ');
+        
+        `}}></script>
+
       </main>
 
       <Footer />
